503ee30ef4d455907d1255af5bdcc20e8e6275d9
zkp <2470904132@qq.com>
2022-09-29 23:45:42 +0800

    init

---
+++ src/main/java/com/example/zkptest/ZkptestApplication.java
@@ -0,0 +1,13 @@
+package com.example.zkptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zkptest/ZkptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zkptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkptestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
